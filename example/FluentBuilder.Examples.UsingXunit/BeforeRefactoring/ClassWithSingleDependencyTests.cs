06a4e76847b0ffd1e4d66998007dd50adb8e8369
Ted Månsken <ted.wik@gmail.com>
2021-10-09 23:58:37 +0200

    Add sample test setups using regular moq

---
+++ example/FluentBuilder.Examples.UsingXunit/BeforeRefactoring/ClassWithSingleDependencyTests.cs
@@ -0,0 +1,27 @@
+﻿using FluentAssertions;
+using FluentBuilder.Examples.Common.Contracts;
+using FluentBuilder.Examples.Common.Dependencies;
+using FluentBuilder.Examples.Common.Sut;
+using Moq;
+using Xunit;
+
+namespace FluentBuilder.Examples.UsingXunit.BeforeRefactoring
+{
+    public class ClassWithSingleDependencyTests
+    {
+        private readonly Mock<IDoSomething> _doSomething;
+        private readonly IContract _sut;
+
+        public ClassWithSingleDependencyTests()
+        {
+            _doSomething = new Mock<IDoSomething>();
+            _sut = new ClassWithSingleDependency(_doSomething.Object);
+        }
+
+        [Fact]
+        public void SutShouldNotBeNull()
+        {
+            _sut.Should().NotBeNull();
+        }
+    }
+}
---
+++ example/FluentBuilder.Examples.UsingXunit/BeforeRefactoring/ClassWithoutDependenciesTests.cs
@@ -0,0 +1,23 @@
+﻿using FluentAssertions;
+using FluentBuilder.Examples.Common.Contracts;
+using FluentBuilder.Examples.Common.Sut;
+using Xunit;
+
+namespace FluentBuilder.Examples.UsingXunit.BeforeRefactoring
+{
+    public class ClassWithoutDependenciesTests
+    {
+        private readonly IContract _sut;
+
+        public ClassWithoutDependenciesTests()
+        {
+            _sut = new ClassWithoutDependencies();
+        }
+
+        [Fact]
+        public void SutShouldNotBeNull()
+        {
+            _sut.Should().NotBeNull();
+        }
+    }
+}
