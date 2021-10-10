bc8ee69e60afd1fee05ce031f95723558a080fee
Ted Månsken <ted.wik@gmail.com>
2021-10-10 23:59:44 +0200

    Create public API entry point BuilderFactory.CreateSutBuilder

---
+++ src/FluentBuilder/v1/BuilderFactory.cs
@@ -0,0 +1,20 @@
+﻿namespace FluentBuilder.v1
+{
+    /// <summary>
+    /// Entry point for FluentBuilder, creates the various tools provided by the package.
+    /// </summary>
+    public static class BuilderFactory
+    {
+        // TODO: Setup SyntaxListener to look for method calls of this method, in order to generate the appropriate builders.
+        /// <summary>
+        /// Create an auto-generated builder for the SUT class <typeparamref name="TSut"/>.
+        /// </summary>
+        /// <typeparam name="TSut">SUT class for which the builder should create.</typeparam>
+        /// <returns>Builder of SUT class <typeparamref name="TSut"/> instances.</returns>
+        public static ISutBuilder<TSut> CreateSutBuilder<TSut>()
+        {
+            // TODO: Implement factory
+            throw new NotImplementedException();
+        }
+    }
+}
