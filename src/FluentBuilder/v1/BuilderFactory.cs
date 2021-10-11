f7c4875a6551185da7c6d704a74d618372e1b82e
Ted Månsken <ted.wik@gmail.com>
2021-10-11 22:01:19 +0200

    Feature/create public api entrypoint (#6)
    * Create public API entry point BuilderFactory.CreateSutBuilder
    
    * Update summary comment of public API
    
    * Add CreateBuilder for regular data models.

---
+++ src/FluentBuilder/v1/BuilderFactory.cs
@@ -0,0 +1,32 @@
+﻿namespace FluentBuilder.v1
+{
+    /// <summary>
+    /// Entry point for FluentBuilder, creates the various tools provided by the package.
+    /// </summary>
+    public static class BuilderFactory
+    {
+        // TODO: Setup SyntaxListener to look for method calls of this method, in order to generate the appropriate builders.
+        /// <summary>
+        /// Create an auto-generated builder of SUT <typeparamref name="TSut"/> instances.
+        /// </summary>
+        /// <typeparam name="TSut">SUT for which the builder should create.</typeparam>
+        /// <returns>New builder of SUT <typeparamref name="TSut"/> instances.</returns>
+        public static ISutBuilder<TSut> CreateSutBuilder<TSut>()
+        {
+            // TODO: Implement factory
+            throw new NotImplementedException();
+        }
+
+        // TODO: Setup SyntaxListener to look for method calls of this method, in order to generate the appropriate builders.
+        /// <summary>
+        /// Create an auto-generated builder of <typeparamref name="TSut"/> instances.
+        /// </summary>
+        /// <typeparam name="T">Type for which the builder should create.</typeparam>
+        /// <returns>New builder of <typeparamref name="TSut"/> instances.</returns>
+        public static IBuilder<T> CreateBuilder<T>()
+        {
+            // TODO: Implement factory
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ src/FluentBuilder/v1/ISutBuilder.cs
@@ -0,0 +1,15 @@
+﻿namespace FluentBuilder.v1
+{
+    /// <summary>
+    /// Builder for SUT, creates instances of <typeparamref name="TSut"/>.
+    /// </summary>
+    /// <typeparam name="TSut">SUT instance.</typeparam>
+    public interface ISutBuilder<TSut>
+    {
+        /// <summary>
+        /// Builds the <typeparamref name="TSut"/> instance as specified.
+        /// </summary>
+        /// <returns>New instance of <typeparamref name="TSut"/>.</returns>
+        public TSut Build();
+    }
+}
