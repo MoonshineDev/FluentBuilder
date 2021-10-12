5a14c111d8929ff9838a5fed2ff3bb3df11d2052
Ted Månsken <ted.wik@gmail.com>
2021-10-12 23:19:46 +0200

    Add generator for ISutBuilder (#28)

---
+++ src/FluentBuilder.SourceGenerators/SutBuilderGenerator.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.CodeAnalysis;
+
+namespace FluentBuilder.SourceGenerators
+{
+    [Generator]
+    public class SutBuilderGenerator : ISourceGenerator
+    {
+        public void Initialize(GeneratorInitializationContext context)
+        {
+            // TODO: #13 Implement SyntaxListener
+        }
+
+        public void Execute(GeneratorExecutionContext context)
+        {
+            // TODO: #27 Implement generator
+            context.AddSource("generator-poc", "[assembly: System.Reflection.AssemblyMetadata(\"SutBuilderGeneratorPOC\", \"success!\")]");
+        }
+    }
+}
---
+++ test/FluentBuilder.SourceGenerators.UnitTests/SutBuilderGeneratorTests.cs
@@ -0,0 +1,20 @@
+﻿using FluentAssertions;
+using System.Reflection;
+using Xunit;
+
+namespace FluentBuilder.SourceGenerators
+{
+    public class SutBuilderGeneratorTests
+    {
+        [Theory]
+        [InlineData("SutBuilderGeneratorPOC", "success!")]
+        public void Assembly_ShouldBeTaggedWithMetadata(string key, string value)
+        {
+            var metadatas = Assembly
+                .GetExecutingAssembly()
+                .GetCustomAttributes<AssemblyMetadataAttribute>();
+
+            metadatas.Should().ContainSingle(_ => _.Key == key && _.Value == value);
+        }
+    }
+}
